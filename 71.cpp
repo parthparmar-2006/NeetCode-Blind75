be0cb4d905e8bb94d68a2e167a6a70ade08c57f7
parthparmar-2006 <parthbparmar2001@gmail.com>
2025-01-13 04:17:09 +0530

    NeetCode.io Blind 75

---
+++ 71.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while (n) {
+            cnt += n&1;
+            n >>= 1;
+        } return cnt;
+    }
+};
